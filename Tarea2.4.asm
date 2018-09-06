eecf6f178fe3bfcd52d9113e1d9b18a3a7c73552
DaniFS <ie709303@iteso.mx>
2018-09-05 23:01:02 -0500

    Commit #6

--- Tarea2.4.asm
+++ Tarea2.4.asm
@@ -2,22 +2,22 @@
 # ITESO. Arquitectura computacional
 
 .text
-	li $t0, 2 # a
-	li $s0, 2 # b
+	li $t0, 15 # a
+	li $s0, 6 # b
 	li $v0, 0 # Aqui se almacena el resultado
 	li $v1, 0 # Variable temporal que pasa a ser t0
 	jal division
 	j exit
 division:
-	slt $t1, $s0, $t0
-	bne $t1, $zero, else # si b>a devuelve un 0
-	# hay un error en elbranch if not equal, slt no reocnoce a -2 como menor a 0
+	slt $t1, $s0, $t0 # Sinceramente, perdi la hilacion de que hacia esta linea y porque va primero s0 que t0. A prueba y error, vi que asi funcionaba
+	beq $t1, $zero, else # si b>a devuelve un 0
 	sub $t0, $t0, $s0 # se resta a-b
-	addi $sp, $sp, -4 # Decrementa el SP y lo deja una posicione smas abajo
+	addi $sp, $sp, -4 # Decrementa el SP y lo deja una posiciones mas abajo
 	sw $ra, 0($sp) # Almacena el lugar previo
 	jal division
-	addi $v0, $v1, 1 # se pone en v0 lo que tenga v1 + 1
+	addi $v0, $v0, 1 # se pone en v0 lo que tenga v1 + 1
 	lw $ra, 0($sp)
+	addi $sp, $sp, 4
 else:
 	li $v1, 0 
 	jr $ra
